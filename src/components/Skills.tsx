c5e65c6b2ba0f5d62d9716181f39bebabf425cea
akku2005 <asakashsahu20@gmail.com>
2024-06-15 23:44:06 +0530

    final

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BackgroundBeams } from "./ui/background-beams";
-import TabsDemo from "./cards/TabsCards"; // Update the import path if necessary
+import TabsDemo from "./cards/TabsCards"; 
 
 const Skills: React.FC = () => {
   return (
-    <section id="skills" className="hero text-white relative">
+    <section id="skills" className="hero text-white relative mt-0 sm:mt-[value]">
       <BackgroundBeams />
-      <div className="container mx-auto px-4 py-20">
+      <div className="container mx-auto px-4 ">
         <h1 className="text-3xl font-bold mb-0 text-center">Skills</h1>
         <TabsDemo />
-        </div>
+      </div>
     </section>
   );
 };
